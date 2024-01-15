17be17e76e6d6d43c4217b230ce231fdfb7d1468
Ryan Silva <ryan.silva@b2ml.com.br>
2024-01-15 08:04:19 -0300

    Adicionando aula de TS realizada na Udemy, feita totalmente no arquivo index.ts

--- Estudos TS/dist/js/index.js
+++ Estudos TS/dist/js/index.js
@@ -1,4 +1,42 @@
 "use strict";
+var __esDecorate = (this && this.__esDecorate) || function (ctor, descriptorIn, decorators, contextIn, initializers, extraInitializers) {
+    function accept(f) { if (f !== void 0 && typeof f !== "function") throw new TypeError("Function expected"); return f; }
+    var kind = contextIn.kind, key = kind === "getter" ? "get" : kind === "setter" ? "set" : "value";
+    var target = !descriptorIn && ctor ? contextIn["static"] ? ctor : ctor.prototype : null;
+    var descriptor = descriptorIn || (target ? Object.getOwnPropertyDescriptor(target, contextIn.name) : {});
+    var _, done = false;
+    for (var i = decorators.length - 1; i >= 0; i--) {
+        var context = {};
+        for (var p in contextIn) context[p] = p === "access" ? {} : contextIn[p];
+        for (var p in contextIn.access) context.access[p] = contextIn.access[p];
+        context.addInitializer = function (f) { if (done) throw new TypeError("Cannot add initializers after decoration has completed"); extraInitializers.push(accept(f || null)); };
+        var result = (0, decorators[i])(kind === "accessor" ? { get: descriptor.get, set: descriptor.set } : descriptor[key], context);
+        if (kind === "accessor") {
+            if (result === void 0) continue;
+            if (result === null || typeof result !== "object") throw new TypeError("Object expected");
+            if (_ = accept(result.get)) descriptor.get = _;
+            if (_ = accept(result.set)) descriptor.set = _;
+            if (_ = accept(result.init)) initializers.unshift(_);
+        }
+        else if (_ = accept(result)) {
+            if (kind === "field") initializers.unshift(_);
+            else descriptor[key] = _;
+        }
+    }
+    if (target) Object.defineProperty(target, contextIn.name, descriptor);
+    done = true;
+};
+var __runInitializers = (this && this.__runInitializers) || function (thisArg, initializers, value) {
+    var useValue = arguments.length > 2;
+    for (var i = 0; i < initializers.length; i++) {
+        value = useValue ? initializers[i].call(thisArg, value) : initializers[i].call(thisArg);
+    }
+    return useValue ? value : void 0;
+};
+var __setFunctionName = (this && this.__setFunctionName) || function (f, name, prefix) {
+    if (typeof name === "symbol") name = name.description ? "[".concat(name.description, "]") : "";
+    return Object.defineProperty(f, "name", { configurable: true, value: prefix ? "".concat(prefix, " ", name) : name });
+};
 //string, boolean, number...
 let x = 10;
 x = 80;
@@ -49,3 +87,148 @@ const camisa = {
     size: Size.g
 };
 console.log(camisa);
+// literal types
+let autenticado;
+autenticado = 'authOK';
+autenticado = null;
+// funçoes
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(12, 12));
+// console.log(sum(false, "12")); não pode
+function sayHelloto(name) {
+    return `Hello ${name}`;
+}
+console.log(sayHelloto("Ryan"));
+// funções que não retornam nada
+function logger(msg) {
+    console.log(msg);
+}
+logger("teste");
+function greeting(name, greet) {
+    if (greet) {
+        console.log(`Olá ${greet} ${name}`);
+        return;
+    }
+    console.log(`Olá ${name}`);
+}
+console.log(greeting("Ryan"));
+console.log(greeting("Pedro", "Sr"));
+function sumNumbers(nums) {
+    return nums.n1 + nums.n2;
+}
+function multiplyNumbers(nums) {
+    return nums.n1 * nums.n2;
+}
+const someNumbers = {
+    n1: 5,
+    n2: 6
+};
+console.log(sumNumbers({ n1: 1, n2: 2 }));
+console.log(multiplyNumbers({ n1: 10, n2: 2 }));
+console.log(multiplyNumbers(someNumbers));
+// Narrowing: checagem de tipos
+function doSomething(info) {
+    if (typeof info === 'number') {
+        console.log(`O número é ${info}`);
+        return;
+    }
+    console.log(`Não foi passado um número, foi passado um valor do tipo: ${typeof (info)}`);
+}
+doSomething(5);
+doSomething(true);
+// generics
+function showArrayItems(arr) {
+    arr.forEach((item) => {
+        console.log(`Item: ${item}`);
+    });
+}
+const a1 = [1, 2, 3];
+const a2 = ["a", "b", "c"];
+showArrayItems(a1);
+showArrayItems(a2);
+//classes
+class User {
+    constructor(name, role, isAproved) {
+        this.name = name;
+        this.role = role;
+        this.isApproved = isAproved;
+    }
+    showUserName() {
+        console.log(`O nome do usuário é ${this.name}`);
+    }
+    showUserRole(canShow) {
+        if (canShow) {
+            console.log(`A role do usuário é: ${this.role}`);
+            return;
+        }
+        console.log("Informação restrita");
+    }
+}
+const zeca = new User("Zéca", "Admin", true);
+console.log(zeca.showUserName());
+console.log(zeca.showUserRole(false));
+console.log(zeca.showUserRole(true));
+class Car {
+    constructor(brand, wheels) {
+        this.brand = brand;
+        this.wheels = wheels;
+    }
+    showBrand() {
+        console.log(`A marca do carro é: ${this.brand}`);
+    }
+    showWheels() {
+        console.log(`A quantidade de rodas do carro é: ${this.wheels}`);
+    }
+}
+const fusca = new Car("VW", 4);
+fusca.showBrand();
+// herança
+class SuperCar extends Car {
+    constructor(brand, wheels, engine) {
+        super(brand, wheels);
+        this.engine = engine;
+    }
+    showEngine() {
+        console.log(`O motor do carro é: ${this.engine}`);
+    }
+}
+const a4 = new SuperCar("Audi", 4, 2.0);
+a4.showBrand();
+a4.showWheels();
+a4.showEngine();
+// Decorators
+function BaseParamters() {
+    return function (constructor) {
+        return class extends constructor {
+            constructor() {
+                super(...arguments);
+                this.id = Math.random();
+                this.createdAt = new Date();
+            }
+        };
+    };
+}
+let Person = (() => {
+    let _classDecorators = [BaseParamters()];
+    let _classDescriptor;
+    let _classExtraInitializers = [];
+    let _classThis;
+    var Person = _classThis = class {
+        constructor(name) {
+            this.name = name;
+        }
+    };
+    __setFunctionName(_classThis, "Person");
+    (() => {
+        const _metadata = typeof Symbol === "function" && Symbol.metadata ? Object.create(null) : void 0;
+        __esDecorate(null, _classDescriptor = { value: _classThis }, _classDecorators, { kind: "class", name: _classThis.name, metadata: _metadata }, null, _classExtraInitializers);
+        Person = _classThis = _classDescriptor.value;
+        if (_metadata) Object.defineProperty(_classThis, Symbol.metadata, { enumerable: true, configurable: true, writable: true, value: _metadata });
+        __runInitializers(_classThis, _classExtraInitializers);
+    })();
+    return Person = _classThis;
+})();
+const sam = new Person("Sam");
+console.log(sam);
--- Estudos TS/src/index.ts
+++ Estudos TS/src/index.ts
@@ -72,3 +72,205 @@ const camisa = {
 
 console.log(camisa);
 
+// literal types
+let autenticado: "authOK" | null;
+
+autenticado = 'authOK';
+autenticado = null;
+
+// funçoes
+
+function sum(a: number, b: number){
+    return a + b;
+}
+console.log(sum(12, 12));
+// console.log(sum(false, "12")); não pode
+
+function sayHelloto(name: string): string{
+    return `Hello ${name}`;
+}
+
+console.log(sayHelloto("Ryan"));
+
+// funções que não retornam nada
+function logger(msg: string): void{
+    console.log(msg);
+}
+
+logger("teste");
+
+function greeting(name: string, greet?: string): void{
+    if(greet){
+        console.log(`Olá ${greet} ${name}`);
+        return;
+    }
+    console.log(`Olá ${name}`);
+    
+}
+
+console.log(greeting("Ryan"));
+console.log(greeting("Pedro", "Sr"));
+
+// interfaces
+
+interface MathFunctionParams {
+    n1: number,
+    n2: number
+}
+
+function sumNumbers(nums: MathFunctionParams){
+    return nums.n1 + nums.n2;
+}
+
+function multiplyNumbers(nums: MathFunctionParams){
+    return nums.n1 * nums.n2;
+}
+
+const someNumbers: MathFunctionParams = {
+    n1: 5,
+    n2: 6
+}
+
+console.log(sumNumbers({n1: 1, n2: 2}));
+console.log(multiplyNumbers({n1: 10, n2: 2}));
+console.log(multiplyNumbers(someNumbers));
+
+// Narrowing: checagem de tipos
+
+function doSomething(info: number | boolean){
+    if(typeof info === 'number'){
+        console.log(`O número é ${info}`);
+        return;
+    }
+    console.log(`Não foi passado um número, foi passado um valor do tipo: ${typeof(info)}`);
+}
+
+doSomething(5);
+doSomething(true);
+
+// generics
+
+function showArrayItems<T>(arr: T[]) {
+    arr.forEach((item)=>{
+        console.log(`Item: ${item}`)
+    })
+}
+
+const a1: number[] = [1, 2, 3];
+const a2: string[] = ["a", "b", "c"]
+
+showArrayItems(a1);
+showArrayItems(a2);
+
+
+//classes
+
+class User {
+    name
+    role
+    isApproved
+
+    constructor(name: string, role: string, isAproved: boolean){
+        this.name = name;
+        this.role = role;
+        this.isApproved = isAproved
+    }
+
+    showUserName(){
+        console.log(`O nome do usuário é ${this.name}`);
+    }
+
+    showUserRole(canShow: boolean){
+        if(canShow){
+            console.log(`A role do usuário é: ${this.role}`)
+            return;
+        }
+        console.log("Informação restrita");
+    }
+}
+
+const zeca = new User("Zéca", "Admin", true);
+console.log(zeca.showUserName())
+console.log(zeca.showUserRole(false))
+console.log(zeca.showUserRole(true))
+
+//interfaces em classes
+
+interface IVehicle {
+    brand: string
+    showBrand(): void
+}
+
+class Car implements IVehicle{
+
+    brand
+    wheels
+
+    constructor(brand: string, wheels: number){
+        this.brand = brand
+        this.wheels = wheels
+    }
+
+    showBrand(): void {
+        console.log(`A marca do carro é: ${this.brand}`);
+    }
+
+    showWheels(): void{
+        console.log(`A quantidade de rodas do carro é: ${this.wheels}`);
+    }
+}
+
+const fusca = new Car("VW", 4);
+fusca.showBrand()
+
+// herança
+
+class SuperCar extends Car{
+    engine
+
+    constructor(brand: string, wheels: number, engine: number){
+        super(brand, wheels)
+        this.engine = engine
+    }
+
+    showEngine(): void{
+        console.log(`O motor do carro é: ${this.engine}`);
+    }
+}
+
+const a4 = new SuperCar("Audi", 4, 2.0);
+
+a4.showBrand();
+a4.showWheels();
+a4.showEngine();
+
+// Decorators
+
+function BaseParamters() {
+    return function <T extends {new (...args: any[]): {}}>(constructor: T) {
+        return class extends constructor {
+            id = Math.random();
+            createdAt = new Date();
+        };
+    };
+}
+
+@BaseParamters()
+
+class Person{
+    name
+
+    constructor(name: string){
+        this.name = name
+    }
+
+}
+
+const sam = new Person("Sam");
+
+console.log(sam);
+
+
+
+
+
